c903fb61933eecf2fac136e080e4be1407d63460
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-12-17 09:24:44 +0300

    guards on progress, now jumping to reactive forms

--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomsBookingComponent } from './rooms-booking/rooms-booking.component';
 import { RoomsComponent } from './rooms.component';
+import { RoomsGuard } from './rooms.guard';
 
 const routes: Routes = [
-  {path: 'rooms', component: RoomsComponent, children:[
+  {path: 'rooms', component: RoomsComponent,canActivateChild: [RoomsGuard], children:[
     {path: ':id', component: RoomsBookingComponent},
   ]},
 ];
